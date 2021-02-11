b8a94ab0f4f9d8b3fc304c34a32b289e8bc451c0
knn <muigni-hazi.mze@cpe.fr>
2021-02-11 19:31:01 +0100

    Ajout de resolver

--- breadcrumbs-app-employer/pages/index.js
+++ breadcrumbs-app-employer/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
-import BottomNav from '../components/Layout/DrawerLeft'
+import BottomNav from '@/components/Layout/DrawerLeft'
 import Header from '../components/Layout/Header'
 
 export default function Home() {
