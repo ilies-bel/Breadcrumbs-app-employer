b5e02c0d54cef822b37d692369939492eefaaea6
mmze <muigni-hazi.mze@sword-group.com>
2021-02-13 07:29:39 +0100

    Ajout de next-Auth

--- breadcrumbs-app-employer/pages/_app.js
+++ breadcrumbs-app-employer/pages/_app.js
@@ -2,13 +2,18 @@ import '../styles/globals.css'
 import Header from '../components/Layout/Header';
 import BottomNav from '../components/Layout/BottomNavigation';
 
+import { providers, signIn, signOut, useSession } from 'next-auth/client';
+
 function MyApp({ Component, pageProps }) {
+  const [ session, loading ] = useSession();
+  console.log(Object.values(providers))
   return (
     <>
     <Header/>
     <div className="container">
     <BottomNav/>
       <main>
+      {!session ? <button onClick={() => signIn()}>Sign in</button> :  <div><p>Signed in as {session.user.email}</p><button onClick={() => signOut()}>Sign out</button></div>}
         <Component {...pageProps} />
       </main>
     </div>
